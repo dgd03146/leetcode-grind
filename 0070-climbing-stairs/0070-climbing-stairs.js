6ba3b50ff9f6e1f64709d177db5e41e56288552c
GeoJung Im <84106842+dgd03146@users.noreply.github.com>
2025-06-10 22:29:55 +0900

    Time: 0 ms (100%), Space: 55.2 MB (5.7%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ 
+
+var climbStairs = function(n) {
+  const dp = { 1: 1, 2: 2 };
+
+for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+
+
+};
